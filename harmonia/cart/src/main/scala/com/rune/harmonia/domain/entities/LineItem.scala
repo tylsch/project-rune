c3e5026bdbbecee509a80a7eaaa378961f2aaaaf
Tyler Schock <schock3736@gmail.com>
2023-04-13 18:31:24 -0500

    Removed update method from LineItem case class since it was not in use

--- harmonia/cart/src/main/scala/com/rune/harmonia/domain/entities/LineItem.scala
+++ harmonia/cart/src/main/scala/com/rune/harmonia/domain/entities/LineItem.scala
@@ -1,6 +1,3 @@
 package com.rune.harmonia.domain.entities
 
-final case class LineItem(quantity: Int, metadata: Option[Map[String, String]]) extends ChildEntity {
-  def update(quantity: Int, metadata: Option[Map[String, String]]): LineItem =
-    copy(quantity = quantity, metadata = metadata)
-}
+final case class LineItem(quantity: Int, metadata: Option[Map[String, String]]) extends ChildEntity
