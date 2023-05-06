41053268f3214784879a8b0b5553ef8f4e3025c6
Tyler Schock <schock3736@gmail.com>
2023-05-05 19:27:43 -0500

    Replaced URL due to Akka deleting the IntegrationSpec file from GitHub

--- harmonia/cart/src/it/scala/com/rune/harmonia/tests/IntegrationSpec.scala
+++ harmonia/cart/src/it/scala/com/rune/harmonia/tests/IntegrationSpec.scala
@@ -3,7 +3,7 @@ package com.rune.harmonia.tests
 import org.scalatest.wordspec.AnyWordSpecLike
 
 // TODO: Build out IntegrationSpec with Test Containers
-//  in same fashion as https://github.com/akka/akka-projection/blob/main/samples/grpc/shopping-cart-service-scala/src/test/scala/shopping/cart/IntegrationSpec.scala
+//  in same fashion as https://github.com/akka/akka-projection/blob/6288ce6be10d8f23bb6546898687d91b7372336d/samples/grpc/shopping-cart-service-scala/src/test/scala/shopping/cart/IntegrationSpec.scala
 
 class IntegrationSpec extends AnyWordSpecLike {
   "run integration test" should {
