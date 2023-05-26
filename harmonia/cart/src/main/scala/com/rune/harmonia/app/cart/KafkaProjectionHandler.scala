64fe025a02c13d54b9303078293556a294b1b0a2
Tyler Schock <schock3736@gmail.com>
2023-05-25 19:24:47 -0500

    Defined Domain Events

--- harmonia/cart/src/main/scala/com/rune/harmonia/app/cart/KafkaProjectionHandler.scala
+++ harmonia/cart/src/main/scala/com/rune/harmonia/app/cart/KafkaProjectionHandler.scala
@@ -1,6 +1,12 @@
 package com.rune.harmonia.app.cart
 
-// TODO: Implement R2DBC projection handler to Kafka
-class KafkaProjectionHandler {
+import akka.Done
+import akka.projection.eventsourced.EventEnvelope
+import akka.projection.scaladsl.Handler
 
+import scala.concurrent.Future
+
+// TODO: Implement R2DBC projection handler to Kafka
+class KafkaProjectionHandler extends Handler[EventEnvelope[Events.Event]] {
+  override def process(envelope: EventEnvelope[Events.Event]): Future[Done] = ???
 }
