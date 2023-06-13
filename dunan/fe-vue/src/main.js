c518e3c27df49f46a1624dd47c242cafe2f66351
Tyler Schock <schock3736@gmail.com>
2023-06-13 18:12:54 -0500

    Setup and imported Prime libraries

--- dunan/fe-vue/src/main.js
+++ dunan/fe-vue/src/main.js
@@ -1,13 +1,19 @@
 import './assets/main.css'
+import 'primevue/resources/themes/tailwind-light/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
+app.use(PrimeVue)
 app.use(createPinia())
 app.use(router)
 
