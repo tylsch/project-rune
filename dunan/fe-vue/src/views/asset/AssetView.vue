d876289ee393114eb8449aa004c541b44edccc8a
Tyler Schock <schock3736@gmail.com>
2023-06-27 20:15:34 -0500

    Leveraged Menu component with PrimeFlex SCSS mixins for styling

--- dunan/fe-vue/src/views/asset/AssetView.vue
+++ dunan/fe-vue/src/views/asset/AssetView.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { ref } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
-const selectedMenuItem = ref('surface-200');
+// TODO: Add route navigation and active selection when user navigates to page from URL
+const items = ref([
+  { label: 'Specs', icon: 'pi pi-fw pi-info-circle' },
+  { label: 'Gallery', icon: 'pi pi-fw pi-images' },
+  { label: 'Part Explorer', icon: 'pi pi-fw pi-wrench' },
+  { label: 'Part Search', icon: 'pi pi-fw pi-search' }
+]);
 
 </script>
 
@@ -15,45 +20,28 @@ const selectedMenuItem = ref('surface-200');
     </div>
   </div>
   <Divider />
-  <!-- TODO: Integrate into router for navigation, migrate to separate component or leverage Menu component with template-->
   <div class="p-fluid flex flex-column lg:flex-row pb-2">
-    <ul class="list-none m-0 p-0 flex flex-row lg:flex-column justify-content-between lg:justify-content-start mb-5 lg:mb-0">
-      <li>
-        <a :class="[selectedMenuItem]" class="p-ripple lg:w-15rem flex align-items-center cursor-pointer p-3 border-round hover:surface-200 transition-duration-150 transition-colors">
-          <i class="pi pi-info-circle md:mr-2 text-600"></i>
-          <span class="font-medium hidden md:block text-700">Specs</span>
-          <span class="p-ink" role="presentation" aria-hidden="true" style="height: 240px; width: 240px; top: -74px; left: -47px;"></span>
-        </a>
-      </li>
-      <li>
-        <a class="p-ripple lg:w-15rem flex align-items-center cursor-pointer p-3 border-round hover:surface-200 transition-duration-150 transition-colors">
-          <i class="pi pi-images md:mr-2 text-600"></i>
-          <span class="font-medium hidden md:block text-700">Gallery</span>
-          <span class="p-ink" role="presentation" aria-hidden="true" style="height: 240px; width: 240px; top: -74px; left: -47px;"></span>
-        </a>
-      </li>
-      <li>
-        <a class="p-ripple lg:w-15rem flex align-items-center cursor-pointer p-3 border-round hover:surface-200 transition-duration-150 transition-colors">
-          <i class="pi pi-wrench md:mr-2 text-600"></i>
-          <span class="font-medium hidden md:block text-700">Part Explorer</span>
-          <span class="p-ink" role="presentation" aria-hidden="true" style="height: 240px; width: 240px; top: -74px; left: -47px;"></span>
-        </a>
-      </li>
-      <li>
-        <a class="p-ripple lg:w-15rem flex align-items-center cursor-pointer p-3 border-round hover:surface-200 transition-duration-150 transition-colors">
-          <i class="pi pi-search md:mr-2 text-600"></i>
-          <span class="font-medium hidden md:block text-700">Part Search</span>
-          <span class="p-ink" role="presentation" aria-hidden="true" style="height: 240px; width: 240px; top: -74px; left: -47px;"></span>
-        </a>
-      </li>
-    </ul>
-    <!-- TODO: Create router view to navigate menu items to Asset views with transition -->
+    <Menu :model="items" />
     <div class="p-5 surface-0 shadow-4 flex-auto xl:ml-5">
       <RouterView />
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style lang='scss' scoped>
+@import 'primeflex/primeflex.scss';
+:deep(.p-menu) {
+  width: unset;
+  border: unset;
+  border-radius: unset;
+}
+:deep(.p-menu-list) {
+  @include styleclass('list-none m-0 p-0 flex flex-row lg:flex-column justify-content-between lg:justify-content-start mb-5 lg:mb-0')
+}
+:deep(.p-menuitem-content) {
+  @include styleclass('border-round-lg')
+}
+:deep(.p-menuitem-link) {
+  @include styleclass('p-ripple lg:w-15rem flex align-items-center p-3')
+}
 </style>
