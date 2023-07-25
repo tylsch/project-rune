88e84715168fa917736873c0386bd6a3138ccbf1
Tyler Schock <schock3736@gmail.com>
2023-07-24 19:33:59 -0500

    Added PrimeVue Directives

--- dunan-v2/highland/src/main.ts
+++ dunan-v2/highland/src/main.ts
@@ -1,14 +1,40 @@
 import './assets/main.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+
+import BadgeDirective from "primevue/badgedirective";
+import ConfirmationService from 'primevue/confirmationservice';
+import DialogService from 'primevue/dialogservice'
+import FocusTrap from 'primevue/focustrap';
+import Ripple from 'primevue/ripple';
+import StyleClass from 'primevue/styleclass';
+import ToastService from 'primevue/toastservice';
+import Tooltip from 'primevue/tooltip';
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
+app.use(PrimeVue, { ripple: true });
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
 app.use(createPinia())
 app.use(router)
+
+app.directive('tooltip', Tooltip);
+app.directive('badge', BadgeDirective);
+app.directive('ripple', Ripple);
+app.directive('styleclass', StyleClass);
+app.directive('focustrap', FocusTrap);
+
 
 app.mount('#app')
