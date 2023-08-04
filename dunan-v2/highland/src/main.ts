82ecc7448608f8002ba7a8320fc69d861a0f5cd2
Tyler Schock <schock3736@gmail.com>
2023-08-03 20:18:12 -0500

    Upgraded primevue, attempted new tailwind theme, no success

--- dunan-v2/highland/src/main.ts
+++ dunan-v2/highland/src/main.ts
@@ -5,14 +5,14 @@ import 'primevue/resources/primevue.css'
 import 'primeicons/primeicons.css'
 
 
-import BadgeDirective from "primevue/badgedirective";
-import ConfirmationService from 'primevue/confirmationservice';
-import DialogService from 'primevue/dialogservice'
-import FocusTrap from 'primevue/focustrap';
-import Ripple from 'primevue/ripple';
-import StyleClass from 'primevue/styleclass';
-import ToastService from 'primevue/toastservice';
-import Tooltip from 'primevue/tooltip';
+// import BadgeDirective from "primevue/badgedirective";
+// // import ConfirmationService from 'primevue/confirmationservice';
+// // import DialogService from 'primevue/dialogservice'
+// import FocusTrap from 'primevue/focustrap';
+// import Ripple from 'primevue/ripple';
+// // import StyleClass from 'primevue/styleclass';
+// // import ToastService from 'primevue/toastservice';
+// import Tooltip from 'primevue/tooltip';
 
 
 import { createApp } from 'vue'
@@ -21,21 +21,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
+// import Tailwind from 'primevue/tailwind'
 
 const app = createApp(App)
 
+// app.use(PrimeVue, { unstyled: true, pt: Tailwind });
 app.use(PrimeVue, { ripple: true });
-app.use(ConfirmationService);
-app.use(ToastService);
-app.use(DialogService);
+// app.use(ConfirmationService);
+// app.use(ToastService);
+// app.use(DialogService);
 app.use(createPinia())
 app.use(router)
 
-app.directive('tooltip', Tooltip);
-app.directive('badge', BadgeDirective);
-app.directive('ripple', Ripple);
-app.directive('styleclass', StyleClass);
-app.directive('focustrap', FocusTrap);
+// app.directive('tooltip', Tooltip);
+// app.directive('badge', BadgeDirective);
+// app.directive('ripple', Ripple);
+// //app.directive('styleclass', StyleClass);
+// app.directive('focustrap', FocusTrap);
 
 
 app.mount('#app')
