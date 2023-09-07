de86a56ff92912c4447bf56f5ef176abcd8fa191
Tyler Schock <schock3736@gmail.com>
2023-09-06 19:37:53 -0500

    fixed format

--- dunan-v2/highland/src/components/__tests__/AssemblyDisplay.cy.ts
+++ dunan-v2/highland/src/components/__tests__/AssemblyDisplay.cy.ts
@@ -1,12 +1,12 @@
 import AssemblyDisplay from "../catalog/assemblydisplay/AssemblyDisplay.vue"
 
 describe('AssemblyDisplay', () => {
-    it('playground', () => {
-      cy.mount(AssemblyDisplay)
-    })
-  
-    it('renders properly', () => {
-      cy.mount(AssemblyDisplay)
-      cy.findByTestId('header-two').should('contain', 'Hello from Assembly Display Component')
-    })
+  it('playground', () => {
+    cy.mount(AssemblyDisplay)
   })
+
+  it('renders properly', () => {
+    cy.mount(AssemblyDisplay)
+    cy.findByTestId('header-two').should('contain', 'Hello from Assembly Display Component')
+  })
+})
